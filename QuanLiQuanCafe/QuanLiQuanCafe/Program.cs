70646a9206e9fe9bdb9a8d29742c392d3ddba0cd
DESKTOP-F8SLR0C\DHDY <dhdatyit.sgu@gmail.com>
2016-12-14 22:45:33 +0700

    definition

--- QuanLiQuanCafe/QuanLiQuanCafe/Program.cs
+++ QuanLiQuanCafe/QuanLiQuanCafe/Program.cs
@@ -8,12 +8,13 @@ namespace QuanLiQuanCafe
 {
     static class Program
     {
+        #region difinition
         /*
          * Code by DCN9909
          * 14/12/2016
          * Phan Mem Quan Ly Quan Cafe 
          */
-         
+        #endregion
 
 
         /// <summary>
